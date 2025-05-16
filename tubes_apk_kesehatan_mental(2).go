ba7f70a19bf1e002a52de621df2a28328fbb24c3
AliAtaurrahman <ali.ataurrahman@gmail.com>
2025-05-16 11:14:09 +0700

    Add files via upload

---
+++ tubes_apk_kesehatan_mental(2).go
@@ -0,0 +1,90 @@
+package main
+import "fmt"
+
+const NMAX int = 100
+
+type status struct{
+	tingkatDepresi, tingkatKecemasan, tingkatStress int
+}
+type tabStatus [NMAX] status
+
+type user struct {
+	idUser, nama string
+}
+type tabUser [NMAX] user
+
+type assesment struct {
+	totalScore int
+	soal string
+}
+type tabTotal [NMAX] assesment // buat nyimpen segala assesment
+
+func selfAssesment(T tabTotal, scale int) int {
+	T[0].soal = ""
+	T[1].soal = ""
+	T[2].soal = ""  /// bua nyimpen pertanyaan
+
+	for i = 0; i < 21; i ++ {
+		fmt.Println(T[i].soal)
+		fmt.Println("0 - tidak pernah/n1 - /n2 - /n3 - /n4 - /n5 - ") // ini skala
+		fmt.Scan(&scale)
+		if scale < 0 || scale > 5 {
+			fmt.Println("Harap berikan skala yang valid")
+		}
+		T[i].totalScore ++
+	}
+	
+	return T[i].totalScore
+}
+
+func () {
+	
+}
+
+func findAssesmentResult(A tabUser, n int, uId string) int { // binary search
+	idx = -1
+	left = 0
+	right = n - 1
+	for left <= right && idx != -1{
+		if A[mid].idUser == idx{
+			idx = mid
+		} else if A[mid].idUser > idx {
+			left
+		} else {
+			right = mid
+		}
+	}
+	return idx
+}
+
+func deleteAssesmentResult(A t) {
+	var i, idx
+}
+
+func urutSkorTotal() { // sort
+	
+}
+
+func main(){
+	var dep, kecemasan, stress, menu int
+	
+	fmt.Print("Status Kesehatan Mental Anda Saat ini(depresi?/kecemasan?/stress?):")
+	fmt.Scan(&dep, &kecemasan, &stress)
+
+	fmt.Println("")
+	fmt.Println("__________________")
+	fmt.Println("1. ")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("_______________")
+	fmt.Println("Pilih 1/2/3/4")
+	fmt.Scan(&menu)
+		if menu == 1 {
+
+		}
+		else if {
+
+	
+		}
+
+}
